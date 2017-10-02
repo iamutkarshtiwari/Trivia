16ea98a7c54fd59031aa461279fadcc39a4569a5
Utkarsh Tiwari <iamutkarshtiwari@gmail.com>
2017-10-02 13:49:38 +0530

    Extra code slashed

--- app/src/main/java/io/github/iamutkarshtiwari/trivia/Leaderboard.java
+++ app/src/main/java/io/github/iamutkarshtiwari/trivia/Leaderboard.java
@@ -52,47 +52,47 @@ public class Leaderboard extends AppCompatActivity {
         recyclerView.setLayoutManager(horizontalLayout);
         recyclerView.setAdapter(recyclerViewVerticalAdapter);
 
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                int totalItemCount = horizontalLayout.getItemCount();
-                int lastVisibleItem = horizontalLayout.findLastVisibleItemPosition();
-                if (!isLoading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
-                    if (mOnLoadMoreListener != null) {
-                        mOnLoadMoreListener.onLoadMore();
-                    }
-                    isLoading = true;
-                }
-            }
-        });
+//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                int totalItemCount = horizontalLayout.getItemCount();
+//                int lastVisibleItem = horizontalLayout.findLastVisibleItemPosition();
+//                if (!isLoading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+//                    if (mOnLoadMoreListener != null) {
+//                        mOnLoadMoreListener.onLoadMore();
+//                    }
+//                    isLoading = true;
+//                }
+//            }
+//        });
 
-        recyclerViewVerticalAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
-            @Override public void onLoadMore() {
-                Log.e("haint", "Load More");
-                mUsers.add(null);
-                mUserAdapter.notifyItemInserted(mUsers.size() - 1);
-                //Load more data for reyclerview
-                new Handler().postDelayed(new Runnable() {
-                    @Override public void run() {
-                        Log.e("haint", "Load More 2");
-                        //Remove loading item
-                        mUsers.remove(mUsers.size() - 1);
-                        mUserAdapter.notifyItemRemoved(mUsers.size());
-                        //Load data
-                        int index = mUsers.size();
-                        int end = index + 20;
-                        for (int i = index; i < end; i++) {
-                            User user = new User();
-                            user.setName("Name " + i);
-                            user.setEmail("alibaba" + i + "@gmail.com");
-                            mUsers.add(user);
-                        }
-                        mUserAdapter.notifyDataSetChanged();
-                        mUserAdapter.setLoaded();
-                    }
-                }, 5000);
-            }
-        });
+//        recyclerViewVerticalAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+//            @Override public void onLoadMore() {
+//                Log.e("haint", "Load More");
+//                mUsers.add(null);
+//                mUserAdapter.notifyItemInserted(mUsers.size() - 1);
+//                //Load more data for reyclerview
+//                new Handler().postDelayed(new Runnable() {
+//                    @Override public void run() {
+//                        Log.e("haint", "Load More 2");
+//                        //Remove loading item
+//                        mUsers.remove(mUsers.size() - 1);
+//                        mUserAdapter.notifyItemRemoved(mUsers.size());
+//                        //Load data
+//                        int index = mUsers.size();
+//                        int end = index + 20;
+//                        for (int i = index; i < end; i++) {
+//                            User user = new User();
+//                            user.setName("Name " + i);
+//                            user.setEmail("alibaba" + i + "@gmail.com");
+//                            mUsers.add(user);
+//                        }
+//                        mUserAdapter.notifyDataSetChanged();
+//                        mUserAdapter.setLoaded();
+//                    }
+//                }, 5000);
+//            }
+//        });
 
 
         // Adding on item click listener to RecyclerView.
@@ -129,12 +129,12 @@ public class Leaderboard extends AppCompatActivity {
 
 
 
-        for (int i = 0; i < 30; i++) {
-            User user = new User();
-            user.setName("Name " + i);
-            user.setEmail("alibaba" + i + "@gmail.com");
-            mUsers.add(user);
-        }
+//        for (int i = 0; i < 30; i++) {
+//            User user = new User();
+//            user.setName("Name " + i);
+//            user.setEmail("alibaba" + i + "@gmail.com");
+//            mUsers.add(user);
+//        }
 
 
     }
--- app/src/main/java/io/github/iamutkarshtiwari/trivia/models/LeaderboardRecyclerViewAdapter.java
+++ app/src/main/java/io/github/iamutkarshtiwari/trivia/models/LeaderboardRecyclerViewAdapter.java
@@ -50,6 +50,7 @@ public class LeaderboardRecyclerViewAdapter extends RecyclerView.Adapter<Leaderb
     public LeaderboardRecyclerViewAdapter() {
     }
 
+
     @Override
     public MyView onCreateViewHolder(ViewGroup parent, int viewType) {
 
@@ -69,86 +70,102 @@ public class LeaderboardRecyclerViewAdapter extends RecyclerView.Adapter<Leaderb
         return 10;
     }
 
-    public void setOnLoadMoreListener(OnLoadMoreListener mOnLoadMoreListener) {
-        this.mOnLoadMoreListener = mOnLoadMoreListener;
-    }
-    @Override public int getItemViewType(int position) {
-        return mUsers.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
-    }
-    @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == VIEW_TYPE_ITEM) {
-            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_user_item, parent, false);
-            return new UserViewHolder(view);
-        } else if (viewType == VIEW_TYPE_LOADING) {
-            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_loading_item, parent, false);
-            return new LoadingViewHolder(view);
-        }
-        return null;
-    }
-    @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof UserViewHolder) {
-            User user = mUsers.get(position);
-            UserViewHolder userViewHolder = (UserViewHolder) holder;
-            userViewHolder.tvName.setText(user.getName());
-            userViewHolder.tvEmailId.setText(user.getEmail());
-        } else if (holder instanceof LoadingViewHolder) {
-            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
-            loadingViewHolder.progressBar.setIndeterminate(true);
-        }
-    }
-    @Override public int getItemCount() {
-        return mUsers == null ? 0 : mUsers.size();
-    }
-    public void setLoaded() {
-        isLoading = false;
-    }
+//    public void setOnLoadMoreListener(OnLoadMoreListener mOnLoadMoreListener) {
+//        this.mOnLoadMoreListener = mOnLoadMoreListener;
+//    }
+//
+//    @Override public int getItemViewType(int position) {
+//        return mUsers.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+//    }
+//
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        if (viewType == VIEW_TYPE_ITEM) {
+//            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_user_item, parent, false);
+//            return new UserViewHolder(view);
+//        } else if (viewType == VIEW_TYPE_LOADING) {
+//            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_loading_item, parent, false);
+//            return new LoadingViewHolder(view);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//        if (holder instanceof UserViewHolder) {
+//            User user = mUsers.get(position);
+//            UserViewHolder userViewHolder = (UserViewHolder) holder;
+//            userViewHolder.tvName.setText(user.getName());
+//            userViewHolder.tvEmailId.setText(user.getEmail());
+//        } else if (holder instanceof LoadingViewHolder) {
+//            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+//            loadingViewHolder.progressBar.setIndeterminate(true);
+//        }
+//    }
+//    @Override
+//    public int getItemCount() {
+//        return mUsers == null ? 0 : mUsers.size();
+//    }
+//
+//    public void setLoaded() {
+//        isLoading = false;
+//    }
 
 }
 
-
-class LeaderboardRecyclerViewAdapter extends RecyclerView.Adapter < LeaderboardRecyclerViewAdapter.ViewHolder > {
-    private final int VIEW_TYPE_ITEM = 0;
-    private final int VIEW_TYPE_LOADING = 1;
-    private OnLoadMoreListener mOnLoadMoreListener;
-    private boolean isLoading;
-    private int visibleThreshold = 5;
-    private int lastVisibleItem, totalItemCount;
-    public LeaderboardRecyclerViewAdapter() {
-
-    }
-    public void setOnLoadMoreListener(OnLoadMoreListener mOnLoadMoreListener) {
-        this.mOnLoadMoreListener = mOnLoadMoreListener;
-    }
-
-    @Override public int getItemViewType(int position) {
-        return mUsers.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == VIEW_TYPE_ITEM) {
-            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_user_item, parent, false);
-            return new UserViewHolder(view);
-        } else if (viewType == VIEW_TYPE_LOADING) {
-            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_loading_item, parent, false);
-            return new LoadingViewHolder(view);
-        }
-        return null;
-    }
-    @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof UserViewHolder) {
-            User user = mUsers.get(position);
-            UserViewHolder userViewHolder = (UserViewHolder) holder;
-            userViewHolder.tvName.setText(user.getName());
-            userViewHolder.tvEmailId.setText(user.getEmail());
-        } else if (holder instanceof LoadingViewHolder) {
-            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
-            loadingViewHolder.progressBar.setIndeterminate(true);
-        }
-    }
-    @Override public int getItemCount() {
-        return mUsers == null ? 0 : mUsers.size();
-    }
-    public void setLoaded() {
-        isLoading = false;
-    }
+//
+//class LeaderboardRecyclerViewAdapter extends RecyclerView.Adapter < LeaderboardRecyclerViewAdapter.ViewHolder > {
+//    private final int VIEW_TYPE_ITEM = 0;
+//    private final int VIEW_TYPE_LOADING = 1;
+//    private OnLoadMoreListener mOnLoadMoreListener;
+//    private boolean isLoading;
+//    private int visibleThreshold = 5;
+//    private int lastVisibleItem, totalItemCount;
+//
+//    public LeaderboardRecyclerViewAdapter() {
+//
+//    }
+//
+//    public void setOnLoadMoreListener(OnLoadMoreListener mOnLoadMoreListener) {
+//        this.mOnLoadMoreListener = mOnLoadMoreListener;
+//    }
+//
+//    @Override
+//    public int getItemViewType(int position) {
+//        return mUsers.get(position) == null ? VIEW_TYPE_LOADING : VIEW_TYPE_ITEM;
+//    }
+//
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        if (viewType == VIEW_TYPE_ITEM) {
+//            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_user_item, parent, false);
+//            return new UserViewHolder(view);
+//        } else if (viewType == VIEW_TYPE_LOADING) {
+//            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.layout_loading_item, parent, false);
+//            return new LoadingViewHolder(view);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//        if (holder instanceof UserViewHolder) {
+//            User user = mUsers.get(position);
+//            UserViewHolder userViewHolder = (UserViewHolder) holder;
+//            userViewHolder.tvName.setText(user.getName());
+//            userViewHolder.tvEmailId.setText(user.getEmail());
+//        } else if (holder instanceof LoadingViewHolder) {
+//            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+//            loadingViewHolder.progressBar.setIndeterminate(true);
+//        }
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return mUsers == null ? 0 : mUsers.size();
+//    }
+//
+//    public void setLoaded() {
+//        isLoading = false;
+//    }
+//}
